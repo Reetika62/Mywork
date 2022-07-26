681531389ee05672f168481cad70eaea0433adf6
Reetika62 <gautamreetika009@gmail.com>
2022-07-26 10:52:31 +0530

    Initial commit

---
+++ app/src/main/java/com/reetika/mywork/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.reetika.mywork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+  class MainActivity AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
