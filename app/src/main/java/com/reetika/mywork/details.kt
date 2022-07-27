024e0dcc804070edbc609d5b597ca760b1736312
Reetika62 <gautamreetika009@gmail.com>
2022-07-27 14:13:15 +0530

    user information
    this repository contains all the user information

--- app/src/main/java/com/reetika/mywork/details.kt
+++ app/src/main/java/com/reetika/mywork/details.kt
@@ -3,9 +3,28 @@ package com.reetika.mywork
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class details : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+ public class details extends AppCompatActivity() {
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    TextView textView;
+
+    @override fun onCreate(savedInstanceState: Bundle?) {
+        protected.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details)
+
+        radioGroup = findviewById(R.id.radioGroup);
+        textView = findViewById(R.id.button_apply);
+
+        Button buttonApply = findViewById (R.Id.button_apply);
+        buttonApply.setOnclick(view v) {
+
+        }
+    });
+
+    public void checkButton(vies v){
+        int radioId = radioGroup.getcheckedRadiobuttonId();
+
+        radioButton=findViewById(radioId);
+        toast.makeText( context:this,"selected Radio Button: "+radioButton.getText(),Toast.LENGTH_SHORT.show())
     }
 }
